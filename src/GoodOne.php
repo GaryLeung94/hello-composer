8ef82dfcec5109a8a8952eedc43dc930b54ede9d
gary <garyleung@xiyanghui.com>
2018-08-14 09:50:35 +0800

    commit

---
+++ src/GoodOne.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiyanghui
+ * Date: 2018/8/14
+ * Time: 上午9:45
+ */
+
+class GoodOne
+{
+
+    public function hello()
+    {
+        return 'Hello! ';
+    }
+}
